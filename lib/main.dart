ef20c110985ebd594f9e8776099bf63cb0a89b19
Lucas Alves Vilela <lucasvilela2014@gmail.com>
2023-04-22 12:25:43 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 import 'globals.dart';
 import 'dart:math';
-
+import 'package:gpt_ihc/leftSideButton.dart';
 void main() {
   runApp(const MyApp());
 }
